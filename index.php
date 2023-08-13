a60f9af3a79493b2d86e8bfd8ff9013be857966e
latingate <gal2016@latingate.com>
2023-08-13 15:25:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
+redirecting to MeetinRoom..
+
 <?php
+
+// Wait for 2 seconds
+sleep(2);
+
 // Redirect to a subfolder
 $redirectURL = "/MeetingRoom"; // Change this to the actual subfolder URL
 header("Location: $redirectURL");
