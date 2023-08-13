3c62a007ea5640196651f7f32858348d10fe0359
latingate <gal2016@latingate.com>
2023-08-13 15:38:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Redirect to a specific URL
 $redirectURL = "/MeetingRoom"; // Change this to the desired URL
-header("Refresh: 2; Location: $redirectURL");
+header("Location: $redirectURL");
 exit(); // Terminate the script
 ?>
